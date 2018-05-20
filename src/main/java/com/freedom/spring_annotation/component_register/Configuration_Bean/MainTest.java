e3de35c5d4248b8f44e0a65003c24aa5cde6c06e
sunli <lisun4@jieyuechina.com>
2018-05-20 13:35:42 +0800

    @Configuration & @Bean

---
+++ src/main/java/com/freedom/spring_annotation/component_register/Configuration_Bean/MainTest.java
@@ -0,0 +1,38 @@
+package com.freedom.spring_annotation.component_register.Configuration_Bean;
+
+import com.freedom.spring_annotation.common.bean.Person;
+import com.freedom.spring_annotation.component_register.Configuration_Bean.config.MainConfig;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+
+    /**
+     * 传统的基于XML注册主键
+     */
+    @Test
+    public void testClassPathXmlApplicationContext(){
+        ApplicationContext applicationContext =  new ClassPathXmlApplicationContext("beans.xml");
+        Person person = (Person)applicationContext.getBean("person");
+        System.out.println(person);
+    }
+
+    /**
+     * 基于配置类 + @Bean注册组件
+     */
+    @Test
+    public void testAnnotationConfigApplicationContext(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+//        Person person = (Person)applicationContext.getBean("person"); //通过组件id查找
+//        System.out.println(person);
+
+        String[] namesForType = applicationContext.getBeanNamesForType(Person.class);  //通过类型查找BeanName
+        for (String name : namesForType) {
+            System.out.println(name);  //打印不同方式指定的组件id
+        }
+    }
+
+    
+}
---
+++ src/main/java/com/freedom/spring_annotation/component_register/README.md
@@ -0,0 +1,11 @@
+**@Configuration**
+告诉Spring这是一个配置类
+
+**@Bean**
+给容器中注册一个Bean
+Bean类型为返回值的类型，默认使用方法名作为bean id
+也可以在@Bean指定组件id
+
+https://docs.spring.io/spring/docs/4.3.9.RELEASE/spring-framework-reference/html/beans.html#beans-java-basic-concepts
+
+
